a6c9750373b1f1b50f1e66a7057003e0d1c8d0d0
allapugacheva <verhovinandrej@gmail.com>
2024-05-20 19:25:36 +0300

    second

--- src/services/SunriseAndSunsetService.jsx
+++ src/services/SunriseAndSunsetService.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8080/api/v1/suntime/sunriseandsunset'
+// const BASE_URL = 'http://localhost:8080/api/v1/suntime/sunriseandsunset'
+
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getAllSunrisesAndSunsets = () => axios.get(BASE_URL)
 
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
 
-  server: {
-    port: 3000
-  }
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    },
+    server: {
+      port: 3000
+    }
+  };
 })
