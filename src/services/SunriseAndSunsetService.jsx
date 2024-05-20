0f4bb793e9338468c91dbfe35f2c1d9a6fcdc38a
allapugacheva <verhovinandrej@gmail.com>
2024-05-20 20:45:56 +0300

    second

--- src/services/SunriseAndSunsetService.jsx
+++ src/services/SunriseAndSunsetService.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.VITE_API_URL;
+
+const BASE_URL = 'http://localhost:8080/api/v1/suntime/sunriseandsunset';
 
 export const getAllSunrisesAndSunsets = () => axios.get(BASE_URL)
 
